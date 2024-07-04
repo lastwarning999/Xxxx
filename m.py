75ff164e7abfaa777c4d22bb0bc3d17f2fdfd346
lastwarning999 <167008968+lastwarning999@users.noreply.github.com>
2024-07-04 23:01:20 +0530

    m.py

--- m.py
+++ m.py
@@ -235,7 +235,7 @@ def handle_bgmi(message):
             target = command[1]
             port = int(command[2])  # Convert time to integer
             time = int(command[3])  # Convert port to integer
-            if time > 121:
+            if time > 980:
                 response = "Error: Time interval must be less than 80."
             else:
                 record_command_logs(user_id, '/bgmi', target, port, time)
